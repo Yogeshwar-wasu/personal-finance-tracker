206146aebd4b60ea180d5d3e4e2ffeb68c620a8b
Yogeshwar-wasu <wasuyogesh45@gmail.com>
2026-01-16 00:32:29 +0530

    Initial commit

--- backend/server.js
+++ backend/server.js
@@ -10,6 +10,13 @@ const advanceRoutes = require("./routes/advance.routes");
 app.use(cors());
 app.use(express.json());
 
+const angularDistPath = path.join(__dirname, '../frontend/dist/frontend'); // replace with your Angular app name
+app.use(express.static(angularDistPath));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(angularDistPath, 'index.html'));
+});
+
 app.use("/transactions", transactionRoutes);
 app.use("/balance", balanceRoutes);
 app.use("/dashboard", dashboardRoutes);
